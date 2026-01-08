bc63bf49f847fc00d2cc1e93aa91d2a00bd7525e
James Grieve <jamesonrgrieve@gmail.com>
2026-01-07 19:47:52 -0700

    Basic class and object implementation;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,37 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
+  class Student {
+    // This is a "greedy" constructor, meaning it takes as much data through parameters as possible.
+    constructor(firstName, lastName, dateOfBirth) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      // If you use the backing field here (this.#dateOfBirth), you are bypassing any validation - if you do this, ensure it is intentional.
+      this.dateOfBirth = dateOfBirth;
+    }
 
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
+    firstName;
+    lastName;
 
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
+    // Backing field for the property referenced in the getter and setter.
+    #dateOfBirth;
+    // Basic getter without any formatting logic.
+    get dateOfBirth() {
+      return this.#dateOfBirth;
+    }
+    // Basic setter without any validation logic.
+    set dateOfBirth(value) {
+      this.#dateOfBirth = value;
+    }
+    // ^ Essentially this is just a really long winded field at this point.
   }
+
+  let newFirstName = await input("Please enter your first name: ")
+  let newLastName = await input("Please enter your last name: ")
+  let newDate = await input("Please enter your date of birth: ")
+
+  const me = new Student(newFirstName, newLastName, newDate);
+
+  output(JSON.stringify(me));
 }
+
