b573c815f74df4e9a34d0a118ff3ed5df6305cc6
James Grieve <jamesonrgrieve@gmail.com>
2026-01-09 19:17:45 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,9 +3,43 @@
 // eslint-disable-next-line no-unused-vars
 async function main() {
   class Pen {
+    constructor(brand, colour) {
+      this.brand = brand;
+      this.colour = colour;
+      this.inkLevel = 100;
+    }
 
+    brand;
+    colour;
+
+    #inkLevel;
+    get inkLevel() {
+      return this.#inkLevel;
+    }
+    set inkLevel(value) {
+      if (value < 0) {
+        throw new Error("The pen is out of ink!");
+      }
+      this.#inkLevel = value;
+    }
+
+    write(charCount) {
+      // output("Before: " + this.#inkLevel);
+      // output(charCount);
+      try {
+        this.inkLevel -= charCount * 0.5;
+      }
+      catch (e) {
+        output(e);
+      }
+      // output("After: " + this.#inkLevel);
+    }
   }
 
-  const myPen = new Pen();
+  const myPen = new Pen("Bic", "Blue");
+  myPen.write(100);
+  myPen.write(42);
+  myPen.write(200);
+  output(myPen.inkLevel);
 }
 
