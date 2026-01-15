6e685bec187372a7bb1cd3ce33dc8fc86da62632
James Grieve <jamesonrgrieve@gmail.com>
2026-01-14 19:30:01 -0700

    Implement pencil;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,27 +4,57 @@
 async function main() {
   class WritingUtensil {
     constructor(brand, colour) {
+      if (this.constructor == WritingUtensil) {
+        throw new Error("Abstract classes cannot be directly instantiated.");
+      }
       this.brand = brand;
       this.colour = colour;
     }
     brand;
     colour;
+
+    write() {
+      throw new Error("Abstract method was not implemented.");
+    }
   }
   class Pencil extends WritingUtensil {
     constructor(brand, colour) {
       super(brand, colour);
       this.length = 19;
+      this.sharp = true;
     }
+
+    sharp;
 
     #length;
     get length() {
-      return this.#length + "cm";
+      return this.#length;
     }
     set length(value) {
+      output(`Sharpening pencil - length before is ${this.length}cm.`);
       if (value < 0) {
         throw new Error("The pencil is gone!");
       }
       this.#length = value;
+    }
+
+    write(charCount) {
+      for (let i = 0; i < charCount; i++) {
+        const random = Math.floor(Math.random() * 100);
+        const broken = random > 95;
+        output(`For character ${i + 1}, generated ${random} which ${broken ? "breaks" : "does not break"} the pencil tip.`);
+        if (broken) {
+          this.sharp = false;
+        }
+        if (!this.sharp) {
+          this.sharpen();
+        }
+      }
+    }
+
+    sharpen() {
+      this.length -= 1;
+      this.sharp = true
     }
   }
   class Pen extends WritingUtensil {
@@ -62,5 +92,8 @@ async function main() {
   myPen.write(42);
   myPen.write(200);
   output(myPen.inkLevel);
+
+  const myPencil = new Pencil("Ticonderoga", "Black");
+  myPencil.write("50");
 }
 
