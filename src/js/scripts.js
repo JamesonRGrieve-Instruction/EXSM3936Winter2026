6d0055a4806d2ad217baed806f301552d6c84f47
James Grieve <jamesonrgrieve@gmail.com>
2026-01-16 19:17:11 -0700

    Base requirements;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,41 +2,55 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  class Student {
+  class Car {
     // This is a "greedy" constructor, meaning it takes as much data through parameters as possible.
-    constructor(firstName, lastName, dateOfBirth) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      // If you use the backing field here (this.#dateOfBirth), you are bypassing any validation - if you do this, ensure it is intentional.
-      this.dateOfBirth = dateOfBirth;
+    constructor(make, model, year, cylinderCount) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.odometer = 0;
+      this.engine = new Engine(cylinderCount);
     }
 
-    firstName;
-    lastName;
+    make;
+    model;
+    odometer;
+    engine;
+    year;
 
-    // Backing field for the property referenced in the getter and setter.
-    #dateOfBirth;
-    // Basic getter without any formatting logic.
-    get dateOfBirth() {
-      return this.#dateOfBirth;
-    }
-    // Basic setter without any validation logic.
-    set dateOfBirth(value) {
-      this.#dateOfBirth = value;
-    }
-    // ^ Essentially this is just a really long winded field at this point.
 
-    introduce() {
-      output(`Hello, my name is ${this.firstName} ${this.lastName}!`);
+    startEngine() {
+      this.engine.isRunning = true;
+    }
+    stopEngine() {
+      this.engine.isRunning = false;
+    }
+    drive(distance) {
+      if (!this.engine.isRunning) {
+        throw new Error("Start the engine before driving!");
+      }
+      this.odometer += distance;
     }
   }
+  class Engine {
+    constructor(cylinderCount) {
+      this.cylinderCount = cylinderCount;
+      this.isRunning = false;
+    }
 
-  let newFirstName = await input("Please enter your first name: ")
-  let newLastName = await input("Please enter your last name: ")
-  let newDate = await input("Please enter your date of birth: ")
+    cylinderCount;
+    isRunning;
+  }
 
-  const me = new Student(newFirstName, newLastName, newDate);
-  me.introduce();
-  output(JSON.stringify(me));
+  const myCar = new Car("Dodge", "Stealth", 1992, 6);
+  myCar.startEngine();
+  myCar.drive(100);
+  myCar.stopEngine();
+  myCar.startEngine();
+  myCar.drive(50);
+  myCar.stopEngine();
+  output(myCar.odometer);
+
+  output(JSON.stringify(myCar));
 }
 
