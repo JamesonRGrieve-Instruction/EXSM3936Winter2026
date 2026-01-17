76eb0ea6aaa86c95d838cfb3a0129e4a3983d067
James Grieve <jamesonrgrieve@gmail.com>
2026-01-16 19:10:13 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,105 +2,58 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  class WritingUtensil {
-    constructor(brand, colour) {
-      if (this.constructor == WritingUtensil) {
+  class Person {
+    constructor(firstName, middleName, lastName) {
+      if (this.constructor == Person) {
         throw new Error("Abstract classes cannot be directly instantiated.");
       }
-      this.brand = brand;
-      this.colour = colour;
+      this.firstName = firstName;
+      this.middleName = middleName;
+      this.lastName = lastName;
     }
-    brand;
-    colour;
+    firstName;
+    middleName;
+    lastName;
 
-    write() {
+    get fullName() {
+      return `${this.firstName} ${this.middleName} ${this.lastName}`;
+    }
+
+    greet() {
       throw new Error("Abstract method was not implemented.");
     }
   }
-  class Pencil extends WritingUtensil {
-    constructor(brand, colour) {
-      super(brand, colour);
-      this.length = 19;
-      this.sharp = true;
+  class Student extends Person {
+    get fullName() {
+      return `${this.firstName} ${this.lastName}`;
     }
-
-    sharp;
-
-    #length;
-    get length() {
-      return this.#length;
-    }
-    set length(value) {
-      output(`Sharpening pencil - length before is ${this.length}cm.`);
-      if (value < 0) {
-        throw new Error("The pencil is gone!");
-      }
-      this.#length = value;
-    }
-
-    get length_inches() {
-      return this.#length * 0.393701;
-    }
-
-    write(charCount) {
-      for (let i = 0; i < charCount; i++) {
-        const random = Math.floor(Math.random() * 100);
-        const broken = random > 95;
-        output(`For character ${i + 1}, generated ${random} which ${broken ? "breaks" : "does not break"} the pencil tip.`);
-        if (broken) {
-          this.sharp = false;
-        }
-        if (!this.sharp) {
-          this.sharpen();
-        }
-      }
-    }
-
-    sharpen() {
-      this.length -= 1;
-      this.sharp = true
+    greet() {
+      return `Hello, my name is ${this.fullName} and I am a student!`;
     }
   }
-  class Pen extends WritingUtensil {
-    constructor(brand, colour) {
-      super(brand, colour);
-      this.inkLevel = 100;
+  class Lawyer extends Person {
+    get fullName() {
+      return `${this.firstName} ${this.middleName[0]}. ${this.lastName}`;
     }
-
-    #inkLevel;
-    get inkLevel() {
-      return this.#inkLevel;
-    }
-    set inkLevel(value) {
-      if (value < 0) {
-        throw new Error("The pen is out of ink!");
-      }
-      this.#inkLevel = value;
-    }
-
-    write(charCount) {
-      // output("Before: " + this.#inkLevel);
-      // output(charCount);
-      try {
-        output("Writing with pen!");
-        this.inkLevel -= charCount * 0.5;
-      }
-      catch (e) {
-        output(e);
-      }
-      // output("After: " + this.#inkLevel);
+    greet() {
+      return `Hello, my name is ${this.middleName} Esq. and I am a lawyer.`;
     }
   }
 
-  const myPen = new Pen("Bic", "Blue");
-  // myPen.write(50);
+  class BusDriver extends Person {
+    greet() {
+      return `Hello, my name is ${this.fullName} and I drive a bus!`;
+    }
+  }
 
-  const myPencil = new Pencil("Ticonderoga", "Black");
-  // myPencil.write(50);
-
-  const myUtensils = [myPen, myPencil];
-  for (utensil of myUtensils) {
-    utensil.write(50);
+  const people = [
+    new Student("John", "Doe", "Smith"),
+    new BusDriver("Jane", "Ashley", "Doe"),
+    new Lawyer("Joseph", "Paul", "Goode"),
+    new BusDriver("Josh", "Edward", "Wilson")
+  ];
+  for (person of people) {
+    output(person.greet());
   }
 }
 
