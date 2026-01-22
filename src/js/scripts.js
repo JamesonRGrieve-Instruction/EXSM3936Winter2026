4aa3b4930df21967693672c8c669931b005f6e42
James Grieve <jamesonrgrieve@gmail.com>
2026-01-21 19:40:06 -0700

    Basic skeleton;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,26 @@
 // const paragraph = document.querySelector("p:nth-of-type(2)");
-const paragraphList = document.querySelectorAll("p");
-console.log(paragraphList);
-console.log(paragraphList[1]);
+const submitButton = document.querySelector("#add button");
+const titleField = document.querySelector("#title");
+const urlField = document.querySelector("#url");
+const tagsField = document.querySelector("#tags");
+const gallery = document.querySelector("main");
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const newFrame = document.createElement("div");
 
-for (paragraph of paragraphList) {
-  paragraph.addEventListener("click", (event) => {
-    // console.log("Click!");
-    // paragraph.innerText = "Updated.";
-    event.target.classList.toggle("selected");
-  });
-}
+  const newTitle = document.createElement("h3");
+  newTitle.innerText = titleField.value;
+  newFrame.appendChild(newTitle);
+
+  const newImage = document.createElement("img");
+  newImage.src = urlField.value;
+  newImage.alt = newTitle.innerText;
+  newImage.title = newTitle.innerText;
+  newFrame.appendChild(newImage);
+
+  const newTags = document.createElement("p");
+  newTags.innerText = tagsField.value.split(",").join(" ");
+  newFrame.appendChild(newTags);
+
+  gallery.appendChild(newFrame);
+});
