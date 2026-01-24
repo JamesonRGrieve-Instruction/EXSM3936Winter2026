1914c3e54e185ea1bba3cabf55638cbfcb532729
James Grieve <jamesonrgrieve@gmail.com>
2026-01-23 19:08:16 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,11 @@
-// const paragraph = document.querySelector("p:nth-of-type(2)");
-const paragraphList = document.querySelectorAll("p");
-console.log(paragraphList);
-console.log(paragraphList[1]);
-
-for (paragraph of paragraphList) {
-  paragraph.addEventListener("click", (event) => {
-    // console.log("Click!");
-    // paragraph.innerText = "Updated.";
-    event.target.classList.toggle("selected");
+const createButton = document.querySelector("#create");
+console.log(createButton);
+createButton.addEventListener("click", (event) => {
+  console.log("click");
+  const newButton = document.createElement("button");
+  newButton.innerText = "Remove Me!";
+  newButton.addEventListener("click", (event) => {
+    event.target.remove();
   });
-}
+  createButton.parentNode.appendChild(newButton);
+});
