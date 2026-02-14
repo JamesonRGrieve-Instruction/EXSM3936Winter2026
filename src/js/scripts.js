3700fde06b81c28dfc836b6acb823d4411655e8e
James Grieve <jamesonrgrieve@gmail.com>
2026-02-13 19:21:57 -0700

    Error handling;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,36 +5,44 @@ const output = document.querySelector("#output");
 
 searchButton.addEventListener("click", async (event) => {
   event.preventDefault();
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchBar.value.toLowerCase()}`).then(res => res.json());
-  output.innerHTML = "";
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchBar.value.toLowerCase()}`)
+      .then(res => res.json());
+    output.innerHTML = "";
 
-  const name = document.createElement("p");
-  name.innerText = response.species.name;
-  output.appendChild(name);
+    const name = document.createElement("p");
+    name.innerText = response.species.name;
+    output.appendChild(name);
 
-  const image = document.createElement("img");
-  image.src = response.sprites.front_default;
-  image.alt = response.species.name;
-  image.title = response.species.name;
-  output.appendChild(image);
+    const image = document.createElement("img");
+    image.src = response.sprites.front_default;
+    image.alt = response.species.name;
+    image.title = response.species.name;
+    output.appendChild(image);
 
-  for (resType of response.types) {
-    const type = document.createElement("p");
-    type.innerText = `Type ${resType.slot}: ${resType.type.name}`;
-    output.appendChild(type);
+    for (resType of response.types) {
+      const type = document.createElement("p");
+      type.innerText = `Type ${resType.slot}: ${resType.type.name}`;
+      output.appendChild(type);
+    }
+
+    const weight = document.createElement("p");
+    weight.innerText = `Weight: ${response.weight}`;
+    output.appendChild(weight);
+
+    const height = document.createElement("p");
+    height.innerText = `Height: ${response.height}`;
+    output.appendChild(height);
+
+
+    const baseExp = document.createElement("p");
+    baseExp.innerText = `Base Exp: ${response.base_experience}`;
+    output.appendChild(baseExp);
+
   }
+  catch {
+    output.innerText = "Not found.";
 
-  const weight = document.createElement("p");
-  weight.innerText = `Weight: ${response.weight}`;
-  output.appendChild(weight);
-
-  const height = document.createElement("p");
-  height.innerText = `Height: ${response.height}`;
-  output.appendChild(height);
-
-
-  const baseExp = document.createElement("p");
-  baseExp.innerText = `Base Exp: ${response.base_experience}`;
-  output.appendChild(baseExp);
+  }
 
 });
