4a792ef8dbb29a585d14520114f4b86f665cc16f
James Grieve <jamesonrgrieve@gmail.com>
2026-02-13 19:27:52 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,48 +1,20 @@
 // const paragraph = document.querySelector("p:nth-of-type(2)");
-const searchBar = document.querySelector("#search");
-const searchButton = document.querySelector("#submit");
-const output = document.querySelector("#output");
-
-searchButton.addEventListener("click", async (event) => {
+const addButton = document.querySelector("#add");
+const resetButton = document.querySelector("#reset");
+const input = document.querySelector("#number");
+const output = document.querySelector("#count");
+let currentValue = Number(localStorage.getItem("number")) || 0;
+output.innerText = currentValue;
+addButton.addEventListener("click", (event) => {
   event.preventDefault();
-  try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchBar.value.toLowerCase()}`)
-      .then(res => res.json());
-    output.innerHTML = "";
+  currentValue += Number(input.value);
+  output.innerText = currentValue;
+  localStorage.setItem("number", currentValue);
+});
 
-    const name = document.createElement("p");
-    name.innerText = response.species.name;
-    output.appendChild(name);
-
-    const image = document.createElement("img");
-    image.src = response.sprites.front_default;
-    image.alt = response.species.name;
-    image.title = response.species.name;
-    output.appendChild(image);
-
-    for (resType of response.types) {
-      const type = document.createElement("p");
-      type.innerText = `Type ${resType.slot}: ${resType.type.name}`;
-      output.appendChild(type);
-    }
-
-    const weight = document.createElement("p");
-    weight.innerText = `Weight: ${response.weight}`;
-    output.appendChild(weight);
-
-    const height = document.createElement("p");
-    height.innerText = `Height: ${response.height}`;
-    output.appendChild(height);
-
-
-    const baseExp = document.createElement("p");
-    baseExp.innerText = `Base Exp: ${response.base_experience}`;
-    output.appendChild(baseExp);
-
-  }
-  catch {
-    output.innerText = "Not found.";
-
-  }
-
+resetButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  currentValue = 0;
+  output.innerText = currentValue;
+  localStorage.setItem("number", 0);
 });
